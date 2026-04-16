2282ce38c3836da9cc395a257436dba091336142
Wellington <Wellington3matos@hotmail.com>
2026-04-16 20:18:30 -0300

    feat: Primeiro commit

---
+++ src/main/java/com/gestaodebibliotecas/gestaodebibliotecas/GestaodebibliotecasApplication.java
@@ -0,0 +1,13 @@
+package com.gestaodebibliotecas.gestaodebibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodebibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodebibliotecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaodebibliotecas/gestaodebibliotecas/GestaodebibliotecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaodebibliotecas.gestaodebibliotecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodebibliotecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
